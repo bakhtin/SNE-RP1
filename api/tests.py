435b018cfb2f3641be6a4a3b0de51fcb63bf32c4
abakhtin <vmanchenko@wooppay.com>
2016-12-30 12:45:26 +0600

    download_from_vk() reworked

--- api/tests.py
+++ api/tests.py
@@ -1,4 +1,6 @@
 from functions import *
 access_token = "4f050c2ec93a5ed968d23d3358d220632ecdbfe9c34beb95971bb05acdd3e75dd8ce3a181bdf181b9a665"
+#from fs.structures.inode import Tree
 
-# download_from_vk(tree=True)
+#t = download_from_vk(tree=True)
+#print Tree.unmarshal(t)
